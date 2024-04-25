66dfbc549e7b8c1cd2a77dd75e5115f8c3bd78b7
PhanNhatLoi <phanloi971@gmail.com>
2024-04-25 16:56:05 +0700

    implement-docker to project

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
 import { configDotenv } from 'dotenv';
 import { AccessTokenStrategy } from './strategies/accessToken.strategy';
 import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
 import { ConfigService } from '@nestjs/config';
 import { UserService } from 'src/user/user.service';
-import { User, UserSchema } from 'src/user/schemas/user.schema';
 import { UserModule } from 'src/user/user.module';
 import { MongodbModule } from 'src/mongodb/mongodb.module';
 configDotenv();
