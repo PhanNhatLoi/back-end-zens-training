b78953e3f40254f40add1988ac23eecd8b6f462b
PhanNhatLoi <phanloi971@gmail.com>
2024-04-26 15:33:16 +0700

    allow write schema.gql file

--- src/auth/auth.resolver.spec.ts
+++ src/auth/auth.resolver.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { MongodbModule } from '../mongodb/mongodb.module';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { SendmailService } from '../sendmail/sendmail.service';
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthResolver],
+      providers: [
+        AuthService,
+        UserService,
+        JwtService,
+        ConfigService,
+        SendmailService,
+        AuthResolver,
+      ],
+      imports: [MongodbModule],
     }).compile();
 
     resolver = module.get<AuthResolver>(AuthResolver);
